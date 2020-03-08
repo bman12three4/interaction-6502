b8f4de3b06c15339b305e88d32219dd81ccaabc5
Byron Lathi <byron.lathi@student.lovett.org>
2020-03-08 14:28:55 -0500

    Added ability for 6502 to read from fpga

--- interface_6502.v
+++ interface_6502.v
@@ -5,24 +5,30 @@ module interface_6502 (
 		input cs,			// Chip Select (Active Low)
 		input [3:0] rs,	// Register select
 		input wren,			// Write enable (Active low)
-		input [7:0] data_in,	// Data bus
+		
+		//inout [7:0] data_bi,	// Data bus
+		inout [7:0] data_bi,
 		
 		output reg [7:0] led // led output
 	);
 	
 	wire chipclk;
-	
 	assign chipclk = clk_ext1 & ~cs;
 	
-	reg [7:0] int_reg [3:0]; // 16 8 bit registers
-	
-	reg [3:0] curr_addr;		// Current address
-	
+	wire [7:0] data_in;
+	wire [7:0] data_out;
 	wire [7:0] ram_out;
 	
-	wire ram_wren;
+	assign data_bi = (wren & chipclk) ? data_out : 8'bZ;
+	assign data_in = data_bi;
 	
-	assign ram_wren = (curr_addr == 4'b1) ? 1'b1 : 1'b0;
+	reg [7:0] int_reg [3:0]; // 16 8 bit registers
+	reg [3:0] curr_addr;		// Current address
+	
+	wire ram_wren;
+	assign ram_wren = (curr_addr == 4'b1) ? ~wren : 1'b0;
+	
+	assign data_out = (curr_addr == 4'b1) ? ram_out : int_reg[curr_addr];
 	
 	ram a (
 		.address (int_reg[0]),
